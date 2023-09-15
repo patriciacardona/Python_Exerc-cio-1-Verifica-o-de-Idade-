45fcfb20ebe4375dffd0eff5b9597716913bc67e
patriciacardona <144853339+patriciacardona@users.noreply.github.com>
2023-09-15 01:53:52 -0300

    Add files via upload

---
+++ Python_exercicio1_idade.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você é um adulto.")
+elif idade >= 13:
+    print("Você é um adolescente.")
+else:
+    print("Você é uma criança.")
